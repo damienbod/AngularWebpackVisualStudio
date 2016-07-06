8ab9f2c4b0e880d1ded591c4ddc1bd067b96cf76
damienbod <damien_bod@hotmail.com>
2016-07-06 17:18:40 +0200

    fixing ngInit

--- Angular2WebpackVisualStudio/src/Angular2WebpackVisualStudio/angular2App/vendor.js
+++ Angular2WebpackVisualStudio/src/Angular2WebpackVisualStudio/angular2App/vendor.js
@@ -1,8 +1,5 @@
 "use strict";
-require('core-js/client/core');
-require('reflect-metadata');
 require('rxjs');
-require('zone.js/dist/zone');
 require('@angular/common');
 require('@angular/compiler');
 require('@angular/core');
@@ -10,6 +7,7 @@ require('@angular/http');
 require('@angular/platform-browser');
 require('@angular/platform-browser-dynamic');
 require('@angular/router');
+require('reflect-metadata');
 require('jquery/src/jquery');
 require('bootstrap/dist/js/bootstrap');
 require('./css/bootstrap.css');
