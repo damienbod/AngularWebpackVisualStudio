6069f3982d1a05df1858c98631427fa362776ff3
damienbod <damien_bod@hotmail.com>
2016-08-12 22:06:17 +0200

    adding the prod, dev switch to the webpack config

--- Angular2WebpackVisualStudio/src/Angular2WebpackVisualStudio/webpack.config.js
+++ Angular2WebpackVisualStudio/src/Angular2WebpackVisualStudio/webpack.config.js
@@ -1,3 +1,10 @@
 /// <binding ProjectOpened='Run - Development' />
 
-module.exports = require('./webpack.dev.js');
+var isProd = (process.env.NODE_ENV === 'production');
+
+if (!isProd) {
+    module.exports = require('./webpack.dev.js');
+} else
+{
+    module.exports = require('./webpack.prod.js');
+}
