a5249f305d04c4e94627bd45382a9597bf907862
FabianGosebrink <1oSzfcWKTk9raYjytkAT>
2016-08-13 13:47:10 +0200

    correct string comparison with type check ;)

--- Angular2WebpackVisualStudio/src/Angular2WebpackVisualStudio/webpack.config.js
+++ Angular2WebpackVisualStudio/src/Angular2WebpackVisualStudio/webpack.config.js
@@ -2,7 +2,7 @@
 
 var environment = (process.env.NODE_ENV || "development").trim();
 
-if (environment == "development") {
+if (environment === "development") {
     module.exports = require('./webpack.dev.js');
 } else {
     module.exports = require('./webpack.prod.js');
