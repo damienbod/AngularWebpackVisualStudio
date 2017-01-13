cc547f5fe6cc581c77a69aacd2dad48e02bdba81
damienbod <damien_bod@hotmail.com>
2017-01-13 11:08:08 +0100

    adding css style for the about component

--- src/Angular2WebpackVisualStudio/angular2App/app/modules/about/components/about.component.ts
+++ src/Angular2WebpackVisualStudio/angular2App/app/modules/about/components/about.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'about-component',
-    templateUrl: 'about.component.html'
+    templateUrl: 'about.component.html',
+    styleUrls: ['about.component.css']
 })
 
 export class AboutComponent {
