39f0b70b824571e0381036b39f717a83c77a1611
damienbod <damien_bod@hotmail.com>
2017-01-13 12:58:35 +0100

    reverting style css component change due to bug in dev build

--- src/Angular2WebpackVisualStudio/angular2App/app/modules/about/components/about.component.ts
+++ src/Angular2WebpackVisualStudio/angular2App/app/modules/about/components/about.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'about-component',
-    templateUrl: 'about.component.html',
-    styleUrls: ['about.component.css']
+    templateUrl: 'about.component.html'
 })
 
 export class AboutComponent {
