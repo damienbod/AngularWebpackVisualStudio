33052f80ad2583357c00eae099f32959e19b70b4
damienbod <damien_bod@hotmail.com>
2017-03-07 06:14:50 +0100

    HMR fix

--- src/Angular2WebpackVisualStudio/angularApp/main.ts
+++ src/Angular2WebpackVisualStudio/angularApp/main.ts
@@ -4,4 +4,10 @@ declare var System: any;
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+// Enables Hot Module Replacement.
+declare var module: any;
+if (module.hot) {
+    module.hot.accept();
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule);
