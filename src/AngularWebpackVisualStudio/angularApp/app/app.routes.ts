3adec5dab6dfc2cdc9817e17d5be268830612c0b
damienbod <damien_bod@hotmail.com>
2018-04-10 14:08:05 +0200

    code clean up

--- src/AngularWebpackVisualStudio/angularApp/app/app.routes.ts
+++ src/AngularWebpackVisualStudio/angularApp/app/app.routes.ts
@@ -1,15 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { 
-		path: '', 
-		redirectTo: 'home', 
-		pathMatch: 'full' 
-	},
-    {
-        path: 'about', 
-		loadChildren: './about/about.module#AboutModule',
-    }
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'about', loadChildren: './about/about.module#AboutModule'}
 ];
 
 export const AppRoutes = RouterModule.forRoot(routes);
