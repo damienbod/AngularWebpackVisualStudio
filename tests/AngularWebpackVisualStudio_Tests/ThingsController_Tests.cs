894d4beb98fb0545b03ec4d6bc9064f733cbe8eb
damienbod <damien_bod@hotmail.com>
2019-09-24 10:04:07 +0200

    fixing tests

--- tests/AngularWebpackVisualStudio_Tests/ThingsController_Tests.cs
+++ tests/AngularWebpackVisualStudio_Tests/ThingsController_Tests.cs
@@ -5,40 +5,51 @@ using System.Threading.Tasks;
 using AngularWebpackVisualStudio;
 using AngularWebpackVisualStudio.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using Xunit;
 
 namespace AngularWebpackVisualStudio_Tests
 {
-    public class ThingsController_Tests
+    public class ThingsController_Tests : IClassFixture<WebApplicationFactory<AngularWebpackVisualStudio.Startup>>
     {
-        private readonly HttpClient _client;
-        public ThingsController_Tests()
-        {
-            var hostBuilder = new WebHostBuilder();
+        private readonly WebApplicationFactory<AngularWebpackVisualStudio.Startup> _factory;
 
-            // Arrange
-            var server = new TestServer(hostBuilder.UseStartup<Startup>());
-            _client = server.CreateClient();
+        public ThingsController_Tests(WebApplicationFactory<AngularWebpackVisualStudio.Startup> factory)
+        {
+            _factory = factory;
         }
+
+        //public ThingsController_Tests()
+        //{
+        //    var hostBuilder = new WebHostBuilder();
+
+        //    // Arrange
+        //    var server = new TestServer(hostBuilder.UseStartup<Startup>());
+        //    _client = server.CreateClient();
+        //}
 
         [Fact]
         public async Task Should_Add_One_Thing_Then_Return_The_Result()
         {
-            var values = new Dictionary<string, string>
+            // Arrange
+            var client = _factory.CreateClient();
+
+            Thing thing = new Thing
             {
-                {"Id", "1"},
-                { "Name", "NetCore"}
+                Id = 1,
+                Name = "thingname"
             };
 
-            var jsonString = JsonConvert.SerializeObject(values);
+            var jsonString = JsonConvert.SerializeObject(thing);
             var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
-            var response = await _client.PostAsync("/api/things", content);
+            var response = await client.PostAsync("/api/things", content);
 
             response.EnsureSuccessStatusCode();
 
-            var responseGet = await _client.GetAsync("/api/things");
+            var responseGet = await client.GetAsync("/api/things");
             responseGet.EnsureSuccessStatusCode();
             var resultsInString = await responseGet.Content.ReadAsStringAsync();
             var restulsInThingsArray = JsonConvert.DeserializeObject<Thing[]>(resultsInString);
